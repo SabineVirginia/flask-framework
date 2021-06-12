35f9ee9ce395dfb8c35afee29f1bec5b25e98be3
SabineVirginia <sabe_fabe@msn.com>
2021-06-12 12:44:14 +0200

    Add venv info

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/bokeh')
+def bokeh():
+    return render_template('bokeh.html')
+
+
 if __name__ == '__main__':
     app.run(port=33507)
